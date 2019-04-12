c5e6d477f3872cf31f04edd2d660cadb6c1a7e31
Josh5k <jking57@academic.rrc.ca>
2019-04-12 00:09:01 -0500

    Proof of Concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ReferencesComponent } from './references/references.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    ReferencesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
