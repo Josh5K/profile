591b6dcf2dec30f6c3aa7c8aebe93161f053b505
Josh5k <jking57@academic.rrc.ca>
2019-04-16 14:24:11 -0500

    Filled in Data and Style tweaks

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
+  JOBS = [
+    {
+      name: "Northfield IT",
+      title: "Jr System Administrator",
+      date: "April 2018 - Present",
+      description: `Northfield IT is an infrastructure management company providing Dev Ops and Networking services to one of the
+                    largest sports entertainment companies in the United States. As part of the Operations Team I was responsible for Front-Line
+                    monitoring of applications, databases and integration environments. When the fires were all out my focus switch to velocity.
+                    Developing automation solutions with Chef & Docker to ensure a secure and consistent environment.
+                   `
+  },
+  {
+    name: "Creators Club",
+    title: "Software Developer",
+    date: "January 2017 - Present",
+    description: `Creators Club is an online content creators managment company aimed to provide clients with sponserships and
+                  a platform to sell their merchendise. After coming on to assist in 2017 I have seen the company grow from just a single client.
+                  I am responsible for development and maintance of our technical systems including our ecommernce site, game servers, and client portfolios.
+                 `
+}
+]
+
+  Northfield_Description:String = `Northfield IT is an infrastructure management company providing Dev Ops and Networking services to one of the
+                                   largest sports entertainment companies in the United States. As part of the Operations Team I was responsible for Front-Line
+                                   monitoring of applications, databases and integration environments. When the fires were all out my focus switch to velocity.
+                                   Developing automation solutions with Chef & Docker to ensure a secure and consistent environment.
+                                   `;
+
+  CreatorsClub_Description:String = `Creators Club is an online content creators managment company aimed to provide clients with sponserships and
+                                     a platform to sell their merchendise. After coming on to assist in 2017 I have seen the company grow from just a single client.
+                                     I am responsible for development and maintance of our technical systems including our ecommernce site, game servers, and client portfolios.
+                                   `;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -7,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReferencesComponent implements OnInit {
 
+  REFS = [
+    {
+      name: "Marcos Reimer",
+      image: "../../assets/images/mreimer.jpg",
+      company: "Cambrien Credit Union",
+      title: "Software Developer",
+      quote: `The speed at which he picks up new technologies is inspiring.
+              You can always tell when you're working with someone who has talent.`
+    },
+    {
+      name: "Spencer McDonald",
+      image: "../../assets/images/sMcDonald.jpg",
+      company: "Metric IT",
+      title: "Software Developer",
+      quote: `Josh is an extremely talented and ambitious developer with a willingness
+              to learn new things and tackle complex problems. I've enjoyed working with Josh
+              in the past and would not hesitate to recommend him to any of my clients and employers.`
+    },
+    {
+      name: "Ryan Roberts",
+      image: "../../assets/images/rroberts.jpg",
+      company: "24-7 Intouch",
+      title: "Software Developer",
+      quote: `Josh takes a great deal of pride in what he does and he works very hard to achieve
+              the exact results the client is looking for. Josh is dedicated, intelligent,
+              responsible, and a great candidate for a software developer position.`
+    },
+    {
+      name: "Nicole Barry",
+      image: "../../assets/images/nbarry.jpg",
+      company: "Red River College",
+      title: "Instructor",
+      quote: `Josh is hard-worker, team-player, and provided thoughtful and thorough work on time.`
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
