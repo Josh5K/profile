ec804113cb1951009d8142e9dbbae9c4b5592873
Josh5k <jking57@academic.rrc.ca>
2019-04-17 22:38:27 -0500

    Improved Responsive Stylings

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -29,17 +29,6 @@ export class ExperienceComponent implements OnInit {
 }
 ]
 
-  Northfield_Description:String = `Northfield IT is an infrastructure management company providing Dev Ops and Networking services to one of the
-                                   largest sports entertainment companies in the United States. As part of the Operations Team I was responsible for Front-Line
-                                   monitoring of applications, databases and integration environments. When the fires were all out my focus switch to velocity.
-                                   Developing automation solutions with Chef & Docker to ensure a secure and consistent environment.
-                                   `;
-
-  CreatorsClub_Description:String = `Creators Club is an online content creators managment company aimed to provide clients with sponserships and
-                                     a platform to sell their merchendise. After coming on to assist in 2017 I have seen the company grow from just a single client.
-                                     I am responsible for development and maintance of our technical systems including our ecommernce site, game servers, and client portfolios.
-                                   `;
-
   constructor() { }
 
   ngOnInit() {
