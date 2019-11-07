dbe18047fe86b15a4ac5aeb072bf94e8e921e5a0
Josh5k <jking57@academic.rrc.ca>
2019-11-07 00:35:06 -0600

    General Updates

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -10,21 +10,21 @@ export class ExperienceComponent implements OnInit {
   JOBS = [
     {
       name: "Northfield IT",
-      title: "Jr System Administrator",
+      title: "Infrastructure Specialist",
       date: "April 2018 - Present",
       description: `Northfield IT is an infrastructure management company providing Dev Ops and Networking services to one of the
                     largest sports entertainment companies in the United States. As part of the Operations Team I was responsible for Front-Line
-                    monitoring of applications, databases and integration environments. When the fires were all out my focus switch to velocity.
-                    Developing automation solutions with Chef & Docker to ensure a secure and consistent environment.
+                    monitoring of applications, databases and integration environments. When the fires were all out my focus switch to velocity by
+                    developing automation solutions using Chef & Kubernetes to ensure a secure and consistent environment.
                    `
   },
   {
     name: "Creators Club",
-    title: "Software Developer",
+    title: "Co-Owner",
     date: "January 2017 - Present",
-    description: `Creators Club is an online content creators managment company aimed to provide clients with sponserships and
-                  a platform to sell their merchendise. After coming on to assist in 2017 I have seen the company grow from just a single client.
-                  I am responsible for development and maintance of our technical systems including our ecommernce site, game servers, and client portfolios.
+    description: `Creators Club is an online content creators managment company aimed to provide clients with the tools to grow their brands.
+                  I am responsible for development and maintance of our technical systems including our ecommernce site, game servers, and client portfolios. I have also pioneered support and triage
+                  tools to allow us to respond quickly to consumer concerns.
                  `
 }
 ]
